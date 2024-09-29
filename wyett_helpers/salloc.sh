d3b07ac05b43988b45a9250d768493b23c223404
Wyett Zeng <w33zeng@watgpu.cs.uwaterloo.ca>
2024-09-29 16:46:36 +0000

    update salloc

--- wyett_helpers/salloc.sh
+++ wyett_helpers/salloc.sh
@@ -1,4 +1,4 @@
-salloc --gres=gpu:4 --cpus-per-task=4 --mem=150G --time=47:00:00
+salloc --gres=gpu:4 --cpus-per-gpu=8 --mem=150G --time=47:00:00
 
 salloc --gres=gpu:2 --cpus-per-task=4 --mem=150G --time=47:00:00
 
