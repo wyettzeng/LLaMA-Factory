2ca2053ce69983727ecbcd5aed1a8f5a47a1c3f7
wyett <wyettzeng@gmail.com>
2024-10-20 00:41:24 -0400

    added new datasets

--- wyett_helpers/create_symbolic_link.sh
+++ wyett_helpers/create_symbolic_link.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-model_name=mistral_instruct_v3
-return_size=1
+models=("mistral_instruct_v3"
+    "llama3_instruct")
+return_sizes=(1 3 5 10 40)
 
-ln -s ~/CodeDPO/evol_llama_factory_dpo_${model_name}_${return_size}.json ~/LLaMA-Factory/data/evol_${model_name}_${return_size}.json
+for model_name in "${models[@]}"
+do
+    for return_size in "${return_sizes[@]}"
+    do
+        ln -s ~/CodeDPO/generated_datasets/evol_llama_factory_dpo_${model_name}_${return_size}.json ~/LLaMA-Factory/data/evol_${model_name}_${return_size}.json
+    done
+done
