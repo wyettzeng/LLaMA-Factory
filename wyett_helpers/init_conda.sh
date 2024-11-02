6babe28125c21ef3cbbdf55834a77700c757f70c
wyett <wyettzeng@gmail.com>
2024-11-02 10:14:44 -0400

    updates

--- wyett_helpers/init_conda.sh
+++ wyett_helpers/init_conda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PYTHONPATH="${PYTHONPATH}:${PWD}"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export DISABLE_VERSION_CHECK=TRUE
 conda init
 conda activate llamaFactory
 # un comment the following if you need to manually install cuda
