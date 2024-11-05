c95824058744f70751a4b394f74d136e41c2de94
wyett-ping <wyettzeng@gmail.com>
2024-11-05 04:15:15 +0000

    updates

--- wyett_helpers/create_symbolic_link.sh
+++ wyett_helpers/create_symbolic_link.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 models=(
-    # "mistral_instruct_v3"
-    "llama3_instruct"
-    "qwen_coder_2.5"
-    "code_qwen_v1.5"
+    "mistral_instruct_v3"
+    # "llama3_instruct"
+    # "qwen_coder_2.5"
+    # "code_qwen_v1.5"
+    "nxcode_cq_orpo"
 )
 
 return_sizes=(0 1 30)
@@ -12,6 +13,6 @@ for model_name in "${models[@]}"
 do
     for return_size in "${return_sizes[@]}"
     do
-        ln -s /data/code_llm/CodeDPO/generated_datasets/dpo_${model_name}_${return_size}.json ./data/${model_name}_${return_size}.json
+        ln -s /data/code_llm/CodeDPO/generated_datasets/dpo_${model_name}_${return_size}.json ./data/dpo_${model_name}_${return_size}.json
     done
 done
