359ed38dc1ca3b55330392ee04aff626b8e182df
wyett-ping <wyettzeng@gmail.com>
2024-12-03 01:30:30 +0000

    update script

--- wyett_helpers/create_symbolic_link.sh
+++ wyett_helpers/create_symbolic_link.sh
@@ -3,15 +3,16 @@ models=(
     # "mistral_instruct_v3"
     # "llama3_instruct"
     # "qwen_coder_2.5"
-    "code_qwen_v1.5"
-    "nxcode_cq_orpo"
+    # "code_qwen_v1.5"
+    # "nxcode_cq_orpo"
+    "cross_models"
 )
 
 return_sizes=(
     "inf"
     "1"
-    "4"
-    "5"
+    # "4"
+    # "5"
 )
 
 for model_name in "${models[@]}"
